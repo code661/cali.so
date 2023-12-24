686d025f2ce46fa3d61f01aad35a3ebadb9d5532
Tan DuZhi <lucky.tanduzhi@gmail.com>
2023-12-25 02:40:23 +0800

    use Suspense

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ Suspense } from 'react'
 
 import { BlogPosts } from '~/app/(main)/blog/BlogPosts'
 import { Headline } from '~/app/(main)/Headline'
@@ -22,7 +22,9 @@ export default function BlogHomePage() {
               <PencilSwooshIcon className="h-5 w-5 flex-none" />
               <span className="ml-2">近期文章</span>
             </h2>
-            <BlogPosts />
+            <Suspense>
+              <BlogPosts />
+            </Suspense>
           </div>
           <aside className="space-y-10 lg:sticky lg:top-8 lg:h-fit lg:pl-16 xl:pl-20">
             <Newsletter />
